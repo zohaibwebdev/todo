a0de5fdb7017c9659bb56c9cd230369cd63e86b1
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2024-06-25 16:24:52 +0500

    Revert "final"
    This reverts commit 4502afefdb5b9f8ad9546111c32ff135fa1b7753.

--- src/context/todo-context/todo-context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-import { ITodoContext, defaultTodos } from "./todo-context.types";
-
-const TodoContext = createContext<ITodoContext>(defaultTodos)
-
-export const TodoProvider = TodoContext.Provider;
-export const useTodoContext = () => useContext(TodoContext);
